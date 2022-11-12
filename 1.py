f6fb8df19ba17ad1a6daa193b7d25c39db2ea304
Aleksandra Tsaregorodtseva <alizandar@rambler.ru>
2022-11-12 23:18:41 +0300

    first

---
+++ 1.py
@@ -0,0 +1,37 @@
+class FlatIterator:
+    def __init__(self, list_of_list):
+        self.list_of_list = list_of_list
+    def __iter__(self):
+        self.func = lambda list_of_list: [el for el_1 in list_of_list for el in el_1]
+        self.right_list = self.func(self.list_of_list)
+        self.counter = 0
+        return self
+    def __next__(self):
+        self.counter+=1
+        if self.counter>=len(self.right_list)+1:
+            raise StopIteration
+        self.next_el = self.right_list[self.counter - 1]
+        return self.next_el
+
+
+def test_1():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+if __name__ == '__main__':
+    test_1()
